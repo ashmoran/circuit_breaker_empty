c0f06e5a3ac33817fcd0058c20df3e8780714ad8
Ash Moran <ash.moran@patchspace.co.uk>
2014-11-20 18:31:36 +0000

    Initial commit

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,3 @@
-require 'webmachine/test'
-
 # Silence debug-level actor shutdown warnings
 Celluloid.logger.level = Logger::Severity::INFO
 
@@ -10,14 +8,5 @@ RSpec.configure do |config|
   config.expect_with :rspec do |c|
     # Disable the `should` syntax
     c.syntax = :expect
-  end
-
-  # https://github.com/celluloid/celluloid/wiki/Gotchas#rspec-magic
-  # Note you MUST set `async: true` on an example group that uses a
-  # Celluloid actor or the examples will fail due to scope checks that
-  # RSpec makes (probably a good thing, actually)
-  config.before(:each, async: true) do |example|
-    Celluloid.shutdown
-    Celluloid.boot
   end
 end
